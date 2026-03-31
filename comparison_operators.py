5e1b260f6b406f9da304fd749d567b529e87f972
Alexander Guerrero <alexisgt499@gmail.com>
2026-03-31 18:54:50 -0500

    comparison_operators

---
+++ comparison_operators.py
@@ -0,0 +1,35 @@
+# Comparison operators
+# All comparison operators return a Boolean value: True or False
+
+# Equal to
+equal_to = 5 == 5
+print(equal_to)
+
+# Not equal to
+not_equal_to = 5 != 3
+print(not_equal_to)
+
+# Greater than
+greater_than = 15 > 11
+print(greater_than)
+
+# Less than
+less_than = 20 < 40
+print(less_than)
+
+# Greater than or equal to
+greater_than_equal_to = 10 >= 10
+print(greater_than_equal_to)
+
+# Less than or equal to
+less_than_equal_to = 20 <= 30
+print(less_than_equal_to)
+
+#Combined calculations
+
+a = 10
+b = 12
+c = 5
+
+comparison = a + b > c
+print(comparison)
