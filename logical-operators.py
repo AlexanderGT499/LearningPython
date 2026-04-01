f83e11709ac6c55e78a4798e92ece336c7e91e04
Alexander Guerrero <alexisgt499@gmail.com>
2026-03-31 20:34:23 -0500

    logical-operators

---
+++ logical-operators.py
@@ -0,0 +1,20 @@
+# In python we have logical operators that are used to compare booleans
+
+# AND
+
+result = True & True # Return True
+result2 = False & True # Return False
+result3 = True & False # Return False
+result4 = False & False # Return False
+
+# OR
+
+result5 = True | True # Return True
+result6 = False | True # Return True
+result7 = True | False # Return True
+result8 = False | False # Return False
+
+# NOT
+
+result9 = not True # Return False
+result10 = not False # Return True
