0089cb32aa66d5dcc5affb21bc5cef46da016b8e
AlexanderGuerrero <alexanderg10@outlook.com>
2026-05-01 16:56:27 -0500

    Modules (#11)
    Co-authored-by: Alexander Guerrero <alexisgt499@gmail.com>

---
+++ Modules/math_operations.py
@@ -0,0 +1,10 @@
+# First of all, what is a module?
+# A module is a Python file that contains functions or variables.
+# It allows you to organize and reuse code.
+
+# Creating functions
+def add(x, y):
+    return x + y
+
+def multiply(x, y):
+    return x * y
